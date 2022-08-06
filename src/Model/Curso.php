9b16934e639ce0c2f22490af976e040af39b4cc5
Isaac de Souza <isaac@quemais.com.br>
2022-08-06 14:18:14 -0300

    refactor: reimplement get feedback function after class extract

--- src/Model/Curso.php
+++ src/Model/Curso.php
@@ -6,7 +6,7 @@ class Curso
 {
     private $nome;
     private $videos;
-    private $feedbacks;
+    private Feedback $feedbacks;
 
     public function __construct(string $nome)
     {
@@ -15,13 +15,10 @@ class Curso
         $this->feedbacks = [];
     }
 
-    public function receberFeedback(int $nota, ?string $depoimento): void
+    public function receberFeedback(Feedback $feedback): void
     {
-        if ($nota < 9 && empty($depoimento)) {
-            throw new \DomainException('Depoimento obrigatório');
-        }
 
-        $this->feedbacks[] = [$nota, $depoimento];
+        $this->feedbacks[] = [$feedback];
     }
 
     public function adicionarVideo(Video $video)
